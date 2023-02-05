07b22fd3d0e99f4264148e10d034e949cf25bcd2
kodsurfer <kodsurfer@ya.ru>
2023-02-06 01:57:28 +0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use clap::Parser;
+use clap::{arg, Parser};
 
 #[derive(Parser)]
 struct Cli {
@@ -9,10 +9,12 @@ struct Cli {
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
