ec5e13e59417b8973357d761524b6daced066778
kodsurfer <kodsurfer@ya.ru>
2023-04-29 01:40:53 +0300

    move find_matches to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,13 @@ fn main() {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
     info!("ended");
-}
-
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line);
-        }
-    }
 }
 
 #[test]
 fn find_a_match() {
     let mut res = Vec::new();
-    find_matches("pam param pam", "param", &mut res);
+    grrs::find_matches("pam param pam", "param", &mut res);
     assert_eq!(res, b"pam param pam\n")
 }
